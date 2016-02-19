0fee1e2caba3b682295c6a691ad6c45df01fff0e
Gwendolyn Williams <gwilliwaite@gmail.com>
2016-02-19 14:49:23 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+var randomNumber = require('./routes/randomNumber');
+//var routes = require('./routes/routes');
+
+var pg = require('pg');
+var connectionString = '';
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + 'ssl';
+} else {
+    connectionString = 'postgres://localhost:5432/Gwen';
+}
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('port', process.env.PORT || 3000);
+
+app.post('/animals', function(req, res) {
+
+    var addAnimal = {
+        animal_type: req.body.animal_type
+    };
+
+    var number = randomNumber(1, 100);
+
+    pg.connect(connectionString, function(err, client, done) {
+        client.query('INSERT INTO animals (animal_type, number_of_animals) VALUES ($1, $2);',
+            [addAnimal.animal_type, number],
+            function(err, result) {
+                done();
+                if(err) {
+                    console.log('Error inserting data: ', err);
+                    res.send(false);
+                } else {
+                    //console.log(res[0]['person_id']);
+                    res.send(addAnimal);
+                }
+            });
+    });
+
+
+});
+
+app.get('/animals', function(req, res) {
+
+    var results = [];
+
+    pg.connect(connectionString, function(err, client, done) {
+        var query = client.query('SELECT * FROM animals');
+
+        //Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        //close connection
+        query.on('end', function() {
+            client.end();
+            console.log(results);
+            return res.json(results);
+        });
+
+        if(err) {
+            console.log(err);
+        }
+
+    });
+});
+
+app.get('/*', function(req, res) {
+    //request and response
+    console.log('here is the request: ', req.params);
+    var file = req.params[0] || '/views/index.html';
+    res.sendFile(path.join(__dirname, './public', file));
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Server is ready on port ' + app.get('port'));
+});
---
+++ public/scripts/clientapp.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $('#submit').on('click', postCurrentAnimal);
+    $('body').on('load', returnAllAnimals());
+    //$('#employee-container').on('click', '.delete-button', deletePerson);
+});
+
+function postCurrentAnimal() {
+    event.preventDefault();
+
+    var values = {};
+    $.each($('#animalForm').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+    });
+
+    console.log(values);
+
+    $('#employeeForm').find('input[type=text]').val('');
+
+    $.ajax({
+        type: 'POST',
+        url: '/animals',
+        data: values,
+        success: function(data) {
+            if(data) {
+                // everything went ok
+                console.log('from server:', data);
+                appendAnimal();
+
+            } else {
+                console.log('error');
+            }
+        }
+    });
+
+}
+
+function appendAnimal() {
+    $.ajax({
+        type: 'GET',
+        url: '/animals',
+        success: function(data) {
+            var animalType = data[data.length-1].animal_type;
+            var numAnimals = data[data.length-1].number_of_animals;
+
+            $('#display-animals').append('<p>' + animalType + ' | quantity: ' + numAnimals + '</p>');
+
+        }
+    })
+}
+
+function returnAllAnimals() {
+    $.ajax({
+        type: 'GET',
+        url: '/animals',
+        success: function(data) {
+
+            data.forEach(function(animal, i) {
+                var animalType = animal.animal_type;
+                var numAnimals = animal.number_of_animals;
+
+                $('#display-animals').append('<p>' + animalType + ' | quantity: ' + numAnimals + '</p>');
+
+            })
+
+        }
+    })
+}
